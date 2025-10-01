143755aab7b625b7f761361456300a2b16360bc9
Petr <kpa80@yandex.ru>
2025-10-01 20:42:28 +0200

    add OrderViewSet and configure API routing for orders

--- backend_transport/urls.py
+++ backend_transport/urls.py
@@ -14,9 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from transport.views import OrderViewSet
+
+router = DefaultRouter()
+router.register(r'orders', OrderViewSet, basename='order')
 
 urlpatterns = [
-    #    path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
--- transport/views.py
+++ transport/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Order
+from .serializers import OrderSerializer
 
 # Create your views here.
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all().prefetch_related("waypoints")
+    serializer_class = OrderSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ["date", "customer_name"]
+    search_fields = ["order_number", "customer_name"]
